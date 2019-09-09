0ccd866c16f32d5ad61cd383688926a4194d02e7
Yemi <yemiadebayo60@gmail.com>
2019-09-09 17:04:33 +0100

    initial structure with React

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './components/stylesheets/App.sass';
+
+function App() {
+  return (
+    <div className="App">
+      <p className="welcome"> Welcome</p>
+    </div>
+  );
+}
+
+export default App;
