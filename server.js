0a5af2f1f1aeb4a28886226d0cae613653542695
Yemi <yemiadebayo60@gmail.com>
2019-09-10 17:16:40 +0100

    amend mongoose connection for heroku

--- server.js
+++ server.js
@@ -10,27 +10,21 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useMongoClient:true}
 );
 
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log('You have connected with MongoDB database');
-})
+}).on('error', (err) => {
+  console.log('Error is: ', err);
+});
 
 import shoesRouter from './routes/shoes';
 import usersRouter from './routes/users';
 
 app.use('/shoes', shoesRouter);
 app.use('/users', usersRouter);
-
-import path from 'path';
-
-app.use(express.static(path.join(__dirname, 'client/build')))
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'))
-})
 
 app.listen(PORT, () => {
   console.log(`auto baby on ${PORT}!`);
